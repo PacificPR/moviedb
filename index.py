c01db7acfa6b7d096fb69417e6987d8a02e6f376
Prithu Goswami <prithugoswami524@gmail.com>
2019-10-27 12:22:53 +0530

    new search route and index page

--- index.py
+++ index.py
@@ -1,25 +1,22 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request, url_for
+from imdb import IMDb
 
 app = Flask(__name__)
 
-@app.route('/',methods=['GET','POST'])
+ia = IMDb()
+
+@app.route('/')
 def index():
-    return render_template("index.html",msg="YO");
+    print(url_for('search'))
+    return render_template("index.html");
 
-@app.route('/SearchResult',methods=['GET','POST'])
+@app.route('/search')
 def search():
-   details=request.form; #Getting an array with details inputted
-   srch=details['search']; #Getting value of search element 
-   return render_template("search.html",srch=srch); #Passing the value of search text field to variable srch, to use in html .
-
-@app.route('/TopRated',methods=['GET','POST']) #the def name is used while using url_for.
-def rated():
-   return render_template("rated.html");  #render template directs to html page rated.html present in templates directory.
-
-@app.route('/Trending',methods=['GET','POST'])
-def trend():
-   return render_template("trend.html");
-
-
-if __name__ =='__main__':
-    app.run()
+    query = request.args.get('q', None)
+    if not query:
+        return ('')
+    else:
+        movies = ia.search_movie(query)
+        mov = [dict(m) for m in movies]
+        results = {'results': mov}
+        return results;
