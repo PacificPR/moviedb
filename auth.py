bd4d3b42bf6dd6fa03ac9260d7fdd0b3f6af167e
Prithu Goswami <prithugoswami524@gmail.com>
2019-11-17 23:46:22 +0530

    add login and signup pages

--- auth.py
+++ auth.py
@@ -1,15 +1,50 @@
-from flask import Blueprint
+from flask import (Blueprint, render_template, redirect,
+        url_for, request, flash)
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import user_info
 from . import db
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return 'Login'
+    return render_template('login.html')
+
+@auth.route('/login', methods=['POST'])
+def login_post():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = user_info.query.filter_by(username=username).first()
+
+    if not user or not check_password_hash(user.password, password): 
+        flash('Please check your login details and try again.')
+        return redirect(url_for('auth.login'))
+
+    return redirect(url_for('main.profile'))
 
 @auth.route('/signup')
 def signup():
-    return 'Signup'
+    return render_template('signup.html')
+
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    user = user_info.query.filter_by(username=username).first()
+
+    if user:
+        return redirect(url_for('auth.signup'))
+
+    new_user = user_info(username=username, password=generate_password_hash(
+        password, method='sha256'))
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(url_for('auth.login'))
 
 @auth.route('/logout')
 def logout():
