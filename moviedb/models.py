220e292fa999e48f277de2bf761ca0b9f14523aa
Prithu Goswami <prithugoswami524@gmail.com>
2019-11-19 23:11:04 +0530

    add new model (fav table)

--- moviedb/models.py
+++ moviedb/models.py
@@ -1,6 +1,11 @@
 from . import db
 
 class user_info(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, primary_key=True)
     password = db.Column(db.String(100))
     username = db.Column(db.String(100), unique=True)
+    fav_genre = db.Column(db.String(100))
+
+class fav(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    tconst = db.Column(db.Integer, primary_key=True)
