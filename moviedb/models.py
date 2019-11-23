80eea220cae0c7094be1d4e7591138fd9d191c20
Prithu Goswami <prithugoswami524@gmail.com>
2019-11-23 17:14:24 +0530

    add recent_fav to user_info model

--- moviedb/models.py
+++ moviedb/models.py
@@ -5,6 +5,7 @@ class user_info(db.Model):
     password = db.Column(db.String(100))
     username = db.Column(db.String(100), unique=True)
     fav_genre = db.Column(db.String(100))
+    recent_fav = db.Column(db.String)
 
 class fav(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
